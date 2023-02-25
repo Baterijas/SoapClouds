3633df68f04821fe41ffe238057ab46d0ac736ef
Raiens P <raiens.p@gmail.com>
2023-02-25 17:06:47 +0200

    commit

--- scripts.js
+++ scripts.js
@@ -6,3 +6,33 @@ function myFunction() {
         x.className = "rc_nav";
     }
 }
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
+
+  var close = document.getElementsByClassName("closebtn");
+  var i;
+
+  for (i = 0; i < close.length; i++) {
+      close[i].onclick = function () {
+          var div = this.parentElement;
+          div.style.opacity = "0";
+          setTimeout(function () { div.style.display = "none"; }, 600);
+      }
+  }
+  
